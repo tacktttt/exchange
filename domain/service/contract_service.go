eaad0f7b8b6e46b0a2953d52ceb830b8598798ee
tacktttt <32606918+mitsuhamian@users.noreply.github.com>
2022-06-06 21:15:58 +0900

    注文のロジックの枠を実装

--- domain/service/contract_service.go
+++ domain/service/contract_service.go
@@ -1,20 +1,75 @@
 package service
 
-import "github.com/tacktttt/exchange/domain/repository"
+import (
+	"context"
+	"database/sql"
 
-type IContractService interface{}
+	"github.com/tacktttt/exchange/domain/entity/contract"
+	"github.com/tacktttt/exchange/domain/entity/execution"
+	"github.com/tacktttt/exchange/domain/entity/order"
+	"github.com/tacktttt/exchange/domain/repository"
+)
+
+type IContractService interface {
+	CreateOrder(order order.IOrder) error
+}
 
 type ContractService struct {
+	ctx                 context.Context
+	con                 *sql.DB
 	orderRepository     repository.IOrderRepository
 	executionRepository repository.IExecutionRepository
 }
 
 func NewContractService(
+	ctx context.Context,
+	con *sql.DB,
 	orderRepository repository.IOrderRepository,
 	executionRepository repository.IExecutionRepository,
 ) *ContractService {
 	return &ContractService{
+		ctx:                 ctx,
+		con:                 con,
 		orderRepository:     orderRepository,
 		executionRepository: executionRepository,
 	}
+}
+
+func (c *ContractService) CreateOrder(order *order.Order) error {
+	switch order.Position() {
+	case "BUY":
+		return nil
+	case "SELL":
+		return c.sellOrder(order)
+	}
+
+	return nil
+}
+
+func (c *ContractService) sellOrder(order *order.Order) error {
+	buyOrders, err := c.orderRepository.GetBuyOrdersBySellAmount(c.con, order.Amount())
+	if err != nil {
+		return err
+	}
+
+	contract := contract.NewContract(order, buyOrders)
+
+	// TODO: create contracts
+
+	err = c.createExecutions(contract.Executions())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *ContractService) createExecutions(executions []*execution.Execution) error {
+	for _, execution := range executions {
+		_, err := c.executionRepository.Create(c.con, execution)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
